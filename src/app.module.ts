4ce92fe84ae9f0839cac6dcd3a38843bea595b56
Developer.CQ <developer.cq.colombia@gmail.com>
2025-05-19 14:13:04 -0500

    feat: update dependencies and modules conexion DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,15 +18,12 @@ import { DataSource } from 'typeorm';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
         entities: [User, Role],
         autoLoadEntities: true,
         synchronize: false,
+        ssl: { rejectUnauthorized: false },
       }),
     }),
 
