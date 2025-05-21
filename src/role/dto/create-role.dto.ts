9c4c32131902f9cb496ed64ab615c1d1e5f8b73c
Developer.CQ <developer.cq.colombia@gmail.com>
2025-05-21 16:19:38 -0500

    cambios en logica del DTO

--- src/role/dto/create-role.dto.ts
+++ src/role/dto/create-role.dto.ts
@@ -1,10 +1,16 @@
-import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, MaxLength, MinLength } from 'class-validator';
 
 export class CreateRoleDto {
+
+
     @IsString()
     @IsNotEmpty()
+    @MaxLength(10)
+    @MinLength(3)
     name: string;
 
     @IsString()
+    @MaxLength(30)
+    @MinLength(10)
     description: string;
 }
