80a9becc1098b725f15e323f204c426f66a1fa23
SebasXayala <juansebastianayala48@gmail.com>
2025-07-21 15:05:23 -0500

    fix: list document controller JwtAuthGuard

--- src/list_document/list_document.controller.ts
+++ src/list_document/list_document.controller.ts
@@ -3,32 +3,38 @@ import { ListDocumentService } from './list_document.service';
 import { CreateListDocumentDto } from './dto/create-list_document.dto';
 import { UpdateListDocumentDto } from './dto/update-list_document.dto';
 import { CandidateJwtAuthGuard } from 'src/auth/guards/candidate-jwt-auth.guard';
+import { UserJwtAuthGuard } from 'src/auth/guards/user-jwt-auth.guard';
 
 @Controller('list_document')
-@UseGuards(CandidateJwtAuthGuard)
+
 export class ListDocumentController {
     constructor(private readonly listDocumentService: ListDocumentService) { }
 
+    @UseGuards(UserJwtAuthGuard)
     @Post()
     create(@Body() createListDocumentDto: CreateListDocumentDto) {
         return this.listDocumentService.create(createListDocumentDto);
     }
 
+    @UseGuards(UserJwtAuthGuard)
     @Get()
     findAll() {
         return this.listDocumentService.findAll();
     }
 
+    @UseGuards(CandidateJwtAuthGuard)
     @Get('profile/:profileId')
     findByProfile(@Param('profileId', ParseIntPipe) profileId: number) {
         return this.listDocumentService.findByProfile(profileId);
     }
 
+    @UseGuards(UserJwtAuthGuard)
     @Get(':profileId/:requiredDocumentId')
     findOne(@Param('profileId') profileId: string, @Param('requiredDocumentId') requiredDocumentId: string,) {
         return this.listDocumentService.findOne(+profileId, +requiredDocumentId);
     }
 
+    @UseGuards(UserJwtAuthGuard)
     @Patch(':profileId/:requiredDocumentId')
     update(@Param('profileId') profileId: string, @Param('requiredDocumentId') requiredDocumentId: string, @Body() updateListDocumentDto: UpdateListDocumentDto,) {
         return this.listDocumentService.update(
@@ -38,6 +44,7 @@ export class ListDocumentController {
         );
     }
 
+    @UseGuards(UserJwtAuthGuard)
     @Delete(':profileId/:requiredDocumentId')
     remove(@Param('profileId') profileId: string, @Param('requiredDocumentId') requiredDocumentId: string,) {
         return this.listDocumentService.remove(+profileId, +requiredDocumentId);
